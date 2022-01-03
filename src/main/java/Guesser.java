e6ef810c454964a11fd3be0fefae1f72e52b1578
Howard Xiao <thx@cs.washington.edu>
2022-01-02 16:47:47 -0800

    Add skeleton for Winter 2022

---
+++ src/main/java/Guesser.java
@@ -0,0 +1,16 @@
+import java.math.BigInteger;
+
+public class Guesser {
+    /**
+     * This method must return the number Chooser c has chosen. c can guess() any
+     * number and tell you whether the number is "correct", "higher", or "lower".
+     *
+     * @param c The "chooser" that has chosen a number you must guess.
+     * @return The number that the "chooser" has chosen
+     */
+    public static BigInteger findNumber(Chooser c) {
+        // Tip: If you're not sure how to work with BigInteger numbers, we encourage
+        // you to look up its Javadoc online.
+        throw new RuntimeException("Remove this line and implement me!");
+    }
+}
